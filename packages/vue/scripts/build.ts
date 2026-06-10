53007c04974bb7a83e9139423983adfcdfc403f2
MunMun Miao <18216142+MunMunMiao@users.noreply.github.com>
2026-06-10 20:48:02 +0800

    feat(vue): add build script

---
+++ packages/vue/scripts/build.ts
@@ -0,0 +1,50 @@
+import dts from 'bun-plugin-dts'
+
+async function build() {
+  await Bun.build({
+    entrypoints: ['./src/index.ts'],
+    outdir: './dist',
+    naming: '[dir]/[name].[ext]',
+    format: 'esm',
+    target: 'browser',
+    minify: false,
+    external: ['vue', '@defjs/core'],
+    plugins: [
+      dts({
+        output: {
+          noBanner: true,
+        },
+        compilationOptions: {
+          preferredConfigPath: './tsconfig.build.json',
+          followSymlinks: false,
+        },
+      }),
+    ],
+  })
+}
+
+async function afterBuild() {
+  await Bun.write('dist/LICENSE', Bun.file('../../LICENSE'))
+  await Bun.write('dist/README.md', Bun.file('./README.md'))
+
+  const packageJson: Record<string, any> = await Bun.file('package.json').json()
+  delete packageJson.devDependencies
+  delete packageJson.scripts
+  packageJson.module = 'index.js'
+  packageJson.typings = 'index.d.ts'
+  packageJson.exports = {
+    './package.json': './package.json',
+    '.': {
+      types: './index.d.ts',
+      default: './index.js',
+    },
+  }
+  await Bun.write('dist/package.json', JSON.stringify(packageJson, undefined, 2))
+}
+
+async function main() {
+  await build()
+  await afterBuild()
+}
+
+main()
